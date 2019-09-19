8733999b75f1c566bda23975d751c75005484a68
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-19 14:36:49 +0300

    add method insertWorker and instance SimpleDateFormat to Worker model

--- src/main/java/org/accounting/database/models/Worker.java
+++ src/main/java/org/accounting/database/models/Worker.java
@@ -1,11 +1,13 @@
 package org.accounting.database.models;
 
+import com.mysql.cj.jdbc.StatementImpl;
 import org.accounting.database.Database;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -14,6 +16,7 @@ public class Worker {
     public String fullName;
     public Date dateOfBirth;
     public String position;
+    public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public Worker(int id, String fullName, Date dateOfBirth, String position) {
         this.id = id;
@@ -42,5 +45,18 @@ public class Worker {
             se.printStackTrace();
         }
         return arrayList;
+    }
+
+    public static void insertWorker(Worker worker) {
+        try {
+            Connection connection = Database.getConnection();
+            Statement statement = connection.createStatement();
+            String query = String.format("INSERT INTO workers VALUES(null,'%s','%s',(SELECT id FROM positions where position='%s'), null)",
+                    worker.fullName, dateFormat.format(worker.dateOfBirth), worker.position);
+            statement.execute(query);
+            worker.id = (int)((StatementImpl) statement).getLastInsertID();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
     }
 }
