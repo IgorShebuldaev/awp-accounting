52418e14a09560f71ede6e159241f04317686c47
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-19 14:30:49 +0300

    change name to IDataManipulator, change method signature

--- src/main/java/org/accounting/forms/WorkBooks/IDataManipulator.java
+++ src/main/java/org/accounting/forms/WorkBooks/IDataManipulator.java
@@ -2,9 +2,9 @@ package org.accounting.forms.WorkBooks;
 
 import org.accounting.forms.models.MainTableModel;
 
-public interface DataManipulator {
+ public interface IDataManipulator {
     MainTableModel fillTable(MainTableModel tableModel);
-    MainTableModel insertData(Object aValue, MainTableModel tableModel, int rowIndex);
+    MainTableModel insertData(Object aValue, MainTableModel tableModel);
     MainTableModel deleteData(Object aValue, MainTableModel tableModel, int rowIndex);
     MainTableModel updateData(Object aValue, MainTableModel tableModel, int rowIndex);
 }
