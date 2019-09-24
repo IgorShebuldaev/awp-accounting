d2d8cc20e3e2cb82bdc609069218fe2f2a7fd287
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-24 15:31:46 +0300

    change type methods and type arguments in IDataManipulator

--- src/main/java/org/accounting/forms/WorkBooks/IDataManipulator.java
+++ src/main/java/org/accounting/forms/WorkBooks/IDataManipulator.java
@@ -1,10 +1,11 @@
 package org.accounting.forms.WorkBooks;
 
+import org.accounting.database.models.Base;
 import org.accounting.forms.models.MainTableModel;
 
- public interface IDataManipulator {
-    MainTableModel fillTable(MainTableModel tableModel);
-    MainTableModel insertData(Object aValue, MainTableModel tableModel);
-    MainTableModel deleteData(Object aValue, MainTableModel tableModel, int rowIndex);
-    MainTableModel updateData(Object aValue, MainTableModel tableModel, int rowIndex);
+ public interface IDataManipulator  {
+    void fillTable(MainTableModel tableModel);
+    void insertData(Base base, MainTableModel tableModel);
+    void deleteData(Base base, MainTableModel tableModel, int rowIndex);
+    void updateData(Base base, MainTableModel tableModel, int rowIndex);
 }
