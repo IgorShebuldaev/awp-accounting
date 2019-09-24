8a92fa0366c5b161d8129f063523badc4f004807
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-24 15:37:14 +0300

    now methods has type void and change type arguments

--- src/main/java/org/accounting/forms/WorkBooks/WorkerImpl.java
+++ src/main/java/org/accounting/forms/WorkBooks/WorkerImpl.java
@@ -1,5 +1,6 @@
 package org.accounting.forms.WorkBooks;
 
+import org.accounting.database.models.Base;
 import org.accounting.database.models.Worker;
 import org.accounting.forms.models.MainTableModel;
 
@@ -11,29 +12,26 @@ public class WorkerImpl implements IDataManipulator {
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
     @Override
-    public MainTableModel fillTable(MainTableModel tableModel) {
-        tableModel.setColumnIdentifiers(new String[]{"Full Name", "Date of birth", "Position"});
-        ArrayList<Worker> results = Worker.getWorkers();
-        for (Worker workers : results) {
-            tableModel.addRow(new Object[]{workers.id, workers.fullName, dateFormat.format(workers.dateOfBirth), workers.position});
+    public void fillTable(MainTableModel tableModel) {
+        ArrayList<Worker> results = Worker.getAll();
+        for (Worker worker : results) {
+            tableModel.addRecord(worker);
         }
-        return tableModel;
     }
 
     @Override
-    public MainTableModel insertData(Object aValue, MainTableModel tableModel) {
-        Worker.insertWorker((Worker) aValue);
-        tableModel.addRow(new Object[]{((Worker) aValue).id, ((Worker) aValue).fullName, dateFormat.format(((Worker) aValue).dateOfBirth), ((Worker) aValue).position});
-        return tableModel;
+    public void insertData(Base base, MainTableModel tableModel) {
+        Worker.insertWorker((Worker) base);
+        tableModel.addRecord(base);
     }
 
     @Override
-    public MainTableModel deleteData(Object aValue, MainTableModel tableModel, int rowIndex) {
-        return null;
+    public void deleteData(Base base, MainTableModel tableModel, int rowIndex) {
+
     }
 
     @Override
-    public MainTableModel updateData(Object aValue, MainTableModel tableModel, int rowIndex) {
-        return null;
+    public void updateData(Base base, MainTableModel tableModel, int rowIndex) {
+
     }
 }
