1c17101c62cc4bcf9818bb6b83213d6df51dac6c
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-25 13:45:25 +0300

    add four fields to get the data of the user who logged in and change sql query

--- src/main/java/org/accounting/database/Authorization.java
+++ src/main/java/org/accounting/database/Authorization.java
@@ -6,14 +6,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Authorization {
+    public int id;
+    public String email;
+    public String role;
+    public int timeInProgram;
+
     public boolean isAuthorized(String login, String password) {
         try {
             Connection connection = Database.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("select email, password from users where email = ?");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT u.id, u.email, u.password, r.role, u.time_in_program FROM users u " +
+                    "INNER JOIN roles r ON u.role_id = r.id WHERE email = ?");
             preparedStatement.setString(1, login);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next() && resultSet.getString(2).equals(password)){
+                id = resultSet.getInt("id");
+                email = resultSet.getString("email");
+                role = resultSet.getString("role");
+                timeInProgram = resultSet.getInt("time_in_program");
                 return true;
             }
 
