33e4390d034773acb66f62582a6a3763fa04c1b6
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-25 13:48:21 +0300

    create gui for MainForm, add timer for current user, generate gui into java source code

--- src/main/java/org/accounting/forms/MainForm.java
+++ src/main/java/org/accounting/forms/MainForm.java
@@ -1,27 +1,43 @@
 package org.accounting.forms;
 
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
 import org.accounting.database.Authorization;
 import org.accounting.database.Database;
 import org.accounting.database.models.Delivery;
+import org.accounting.database.models.User;
 import org.accounting.forms.workbooks.WorkBooksForm;
 import org.accounting.forms.models.DeliveryTable;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class MainForm implements ActionListener {
+public class MainForm extends JFrame implements ActionListener {
     private JFrame authorizationFrame = new JFrame("Log in");
-    private JFrame mainFrame = new JFrame("Accounting");
-
     private JTextField textField = new JTextField(20);
     private JPasswordField passwordField = new JPasswordField(20);
 
-    private JTable tableDeliveries = new JTable();
-    private JScrollPane scrollPaneTableDeliveries = new JScrollPane(tableDeliveries);
+    private JTable tableDelivery;
+    private JPanel panelMain;
+    private JScrollPane scrollPaneMain;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JButton deleteButton;
+    private JSpinner spinnerDelivery;
+    private JComboBox comboBoxSupplier;
+    private JTextField textFieldProduct;
+    private JTextField textFieldPrice;
+    private JComboBox comboBoxWorker;
+    private JLabel labelBar;
 
-    void createUserAuthorizationForm() {
+    private DeliveryTable deliveryTableModel;
+
+    public void createUserAuthorizationForm() {
         JPanel jPanel = new JPanel();
 
         JButton okButton = new JButton("Ok");
@@ -50,7 +66,7 @@ public class MainForm implements ActionListener {
             @Override
             public void keyPressed(KeyEvent keyEvent) {
                 if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
-                    validateUserAuthorization(textField,passwordField);
+                    validateUserAuthorization(textField, passwordField);
                 }
             }
 
@@ -64,16 +80,18 @@ public class MainForm implements ActionListener {
         exitButton.addActionListener(this);
     }
 
-    private void createMainForm() {
-        mainFrame.setJMenuBar(creatMenuBar());
-        mainFrame.setSize(800, 600);
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setVisible(true);
+    private void createMainForm(Authorization currentUser) {
+        this.setJMenuBar(creatMenuBar());
+        this.setContentPane(panelMain);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
 
-        mainFrame.add(scrollPaneTableDeliveries);
+        deliveryTableModel = new DeliveryTable();
         fillTableDeliveries();
+        tableDelivery.setModel(deliveryTableModel);
 
-        mainFrame.addWindowListener(new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 int x = JOptionPane.showConfirmDialog(
                         null,
@@ -86,12 +104,22 @@ public class MainForm implements ActionListener {
                     } catch (SQLException se) {
                         se.printStackTrace();
                     }
-                    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 } else {
-                    mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
                 }
             }
         });
+
+        Timer timer = new Timer(1000, e -> {
+            currentUser.timeInProgram += 1;
+            User.updateTimeUser(currentUser);
+            int seconds = currentUser.timeInProgram % 60;
+            int minutes = currentUser.timeInProgram / 60 % 60;
+            int days = currentUser.timeInProgram / 86400;
+            labelBar.setText("User: " + currentUser.email + ". Role: " + currentUser.role + ". Time in program = " + days + ":" + minutes + ":" + seconds);
+        });
+        timer.start();
     }
 
     private JMenuBar creatMenuBar() {
@@ -134,12 +162,11 @@ public class MainForm implements ActionListener {
     }
 
     private void fillTableDeliveries() {
-        DeliveryTable model = new DeliveryTable();
         ArrayList<Delivery> results = Delivery.getAll();
-        for (Delivery delivery : results){
-            model.addRecord(delivery);
+        for (Delivery delivery : results) {
+            deliveryTableModel.addRecord(delivery);
         }
-        tableDeliveries.setModel(model);
+        tableDelivery.setModel(deliveryTableModel);
     }
 
     @Override
@@ -169,11 +196,72 @@ public class MainForm implements ActionListener {
     }
 
     private void validateUserAuthorization(JTextField login, JPasswordField password) {
-        if (new Authorization().isAuthorized(login.getText(), String.valueOf(password.getPassword()))) {
+        Authorization currentUser = new Authorization();
+        if (currentUser.isAuthorized(login.getText(), String.valueOf(password.getPassword()))) {
             authorizationFrame.dispose();
-            createMainForm();
+            createMainForm(currentUser);
         } else {
             JOptionPane.showMessageDialog(authorizationFrame, "Invalid login or password! Try again.");
         }
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        panelMain = new JPanel();
+        panelMain.setLayout(new GridLayoutManager(4, 5, new Insets(0, 0, 0, 0), -1, -1));
+        scrollPaneMain = new JScrollPane();
+        panelMain.add(scrollPaneMain, new GridConstraints(0, 0, 1, 5, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        tableDelivery = new JTable();
+        scrollPaneMain.setViewportView(tableDelivery);
+        labelBar = new JLabel();
+        labelBar.setText("Label");
+        panelMain.add(labelBar, new GridConstraints(3, 0, 1, 5, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        addButton = new JButton();
+        addButton.setText("Add");
+        panelMain.add(addButton, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        editButton = new JButton();
+        editButton.setText("Edit");
+        panelMain.add(editButton, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        saveButton = new JButton();
+        saveButton.setEnabled(false);
+        saveButton.setText("Save");
+        panelMain.add(saveButton, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        cancelButton = new JButton();
+        cancelButton.setEnabled(false);
+        cancelButton.setText("Cancel");
+        panelMain.add(cancelButton, new GridConstraints(2, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        deleteButton = new JButton();
+        deleteButton.setText("Delete");
+        panelMain.add(deleteButton, new GridConstraints(2, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        spinnerDelivery = new JSpinner();
+        panelMain.add(spinnerDelivery, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        comboBoxSupplier = new JComboBox();
+        panelMain.add(comboBoxSupplier, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        textFieldProduct = new JTextField();
+        panelMain.add(textFieldProduct, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        textFieldPrice = new JTextField();
+        panelMain.add(textFieldPrice, new GridConstraints(1, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        comboBoxWorker = new JComboBox();
+        panelMain.add(comboBoxWorker, new GridConstraints(1, 4, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panelMain;
     }
 }
