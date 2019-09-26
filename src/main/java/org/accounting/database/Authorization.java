5dd50d23c2a1f35dca0366cde17c36cb77933cc1
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-26 10:23:30 +0300

    now method sends the current user's data to the CurrentUser class from Authorization class

--- src/main/java/org/accounting/database/Authorization.java
+++ src/main/java/org/accounting/database/Authorization.java
@@ -1,29 +1,25 @@
 package org.accounting.database;
 
+import org.accounting.user.CurrentUser;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Authorization {
-    public int id;
-    public String email;
-    public String role;
-    public int timeInProgram;
 
     public boolean isAuthorized(String login, String password) {
         try {
             Connection connection = Database.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT u.id, u.email, u.password, r.role, u.time_in_program FROM users u " +
-                    "INNER JOIN roles r ON u.role_id = r.id WHERE email = ?");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT u.id, u.email, u.password, r.role, u.time_in_program " +
+                    "FROM users u INNER JOIN roles r ON u.role_id = r.id WHERE email = ?");
             preparedStatement.setString(1, login);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next() && resultSet.getString(2).equals(password)){
-                id = resultSet.getInt("id");
-                email = resultSet.getString("email");
-                role = resultSet.getString("role");
-                timeInProgram = resultSet.getInt("time_in_program");
+                CurrentUser.setCurrentUser(resultSet.getInt("id"), resultSet.getString("email"),
+                        resultSet.getString("role"), resultSet.getInt("time_in_program"));
                 return true;
             }
 
