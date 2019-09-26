542e37c525ed5eee1ce51fe9ab855af889369be2
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-26 10:11:48 +0300

    now the method returns note only user who is logged in at the moment in Note model, and create method for update note current user

--- src/main/java/org/accounting/database/models/Note.java
+++ src/main/java/org/accounting/database/models/Note.java
@@ -16,23 +16,34 @@ public class Note extends Base {
         this.note = note;
     }
 
-    public static ArrayList<Note> getAll() {
-        ArrayList<Note> results = new ArrayList<>();
+    public static Note getNoteCurrentUser(int id) {
+        Note results = null;
         try {
             Connection connection = Database.getConnection();
             Statement statement = connection.createStatement();
-            String query = "SELECT * FROM notes";
+            String query = String.format("SELECT n.id, n.note FROM workers w INNER JOIN notes n on w.note_id = n.id where w.id=%d", id);
             ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
-                results.add(new Note(
+                results = new Note(
                         resultSet.getInt("id"),
                         resultSet.getString("note")
-                ));
+                );
             }
         } catch (SQLException se) {
             se.printStackTrace();
         }
         return results;
+    }
+
+    public static void updateNote(int id, String note) {
+        try {
+            Connection connection = Database.getConnection();
+            Statement statement = connection.createStatement();
+            String query = String.format("UPDATE notes SET note='%s' WHERE id=(SELECT n.id FROM workers w INNER JOIN notes n on w.note_id = n.id where w.id=%d)", note, id);
+            statement.execute(query);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
     }
 }
