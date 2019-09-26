f2d53238b1283ed3e9f6323f9a8751b782ae301a
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-09-26 12:07:15 +0300

    add method for return record and instance date format in DeliveryTable model

--- src/main/java/org/accounting/forms/models/DeliveryTable.java
+++ src/main/java/org/accounting/forms/models/DeliveryTable.java
@@ -2,7 +2,10 @@ package org.accounting.forms.models;
 
 import org.accounting.database.models.Delivery;
 
+import java.text.SimpleDateFormat;
+
 public class DeliveryTable extends MainTableModel {
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
     public DeliveryTable() {
         columnNames = new String[]{"Date delivery", "Company name", "Product", "Price", "Worker"};
@@ -14,7 +17,7 @@ public class DeliveryTable extends MainTableModel {
         Delivery delivery = (Delivery) data.get(rowIndex);
         switch (columnIndex) {
             case 0:
-                result = delivery.deliveryDate;
+                result = dateFormat.format(delivery.deliveryDate);
                 break;
             case 1:
                 result = delivery.supplier;
@@ -30,5 +33,10 @@ public class DeliveryTable extends MainTableModel {
                 break;
         }
         return result;
+    }
+
+    @Override
+    public Delivery getRecord(int rowIndex) {
+        return (Delivery) super.getRecord(rowIndex);
     }
 }
