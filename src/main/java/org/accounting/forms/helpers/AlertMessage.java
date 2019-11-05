7397ce75d73c0d486a591ec36a5dbd2c3a8bbdfe
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-11-05 16:02:02 +0300

    create function confirmation dialog

--- src/main/java/org/accounting/forms/helpers/AlertMessage.java
+++ src/main/java/org/accounting/forms/helpers/AlertMessage.java
@@ -1,12 +1,12 @@
 package org.accounting.forms.helpers;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class AlertMessage {
     private final String title;
     private String header = null;
     private final String message;
-
 
     public AlertMessage(String title, String message) {
         this.title = title;
@@ -19,13 +19,21 @@ public class AlertMessage {
         this.message = message;
     }
 
-
     public void showErrorMessage() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+
+    public boolean confirmationMessage() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+
+        return alert.showAndWait().get() == ButtonType.OK;
     }
 
 }
