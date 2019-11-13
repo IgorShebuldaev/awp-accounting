a8048f995f40c8c56aabe29cb786684843101d8b
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-11-13 20:31:58 +0300

    when edit the date in the table, the date is converted to the eu format,

--- src/main/java/org/accounting/forms/components/DateTableCell.java
+++ src/main/java/org/accounting/forms/components/DateTableCell.java
@@ -2,11 +2,14 @@ package org.accounting.forms.components;
 
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
+import javafx.util.StringConverter;
 import org.accounting.forms.models.tablemodels.DeliveryFX;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateTableCell extends TableCell<DeliveryFX, Date> {
@@ -32,7 +35,7 @@ public class DateTableCell extends TableCell<DeliveryFX, Date> {
     public void cancelEdit() {
         super.cancelEdit();
 
-        setText(getDate().toString());
+        setText(dateFormat.format(getDate()));
         setGraphic(null);
     }
 
@@ -46,7 +49,7 @@ public class DateTableCell extends TableCell<DeliveryFX, Date> {
         } else {
             if (isEditing()) {
                 if (datePicker != null) {
-                    datePicker.setValue(getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                    datePicker.setValue(convertDateToLocaleDate(getDate()));
                 }
                 setText(null);
                 setGraphic(datePicker);
@@ -58,10 +61,41 @@ public class DateTableCell extends TableCell<DeliveryFX, Date> {
     }
 
     private void createDatePicker() {
-        datePicker = new DatePicker(Instant.ofEpochMilli(getDate().getTime()).atZone(ZoneId.systemDefault()).toLocalDate());
+        datePicker = new DatePicker(convertDateToLocaleDate(getDate()));
+
         datePicker.setOnAction((e) -> {
-            commitEdit(Date.from(datePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            if (datePicker.getValue() != null) {
+                commitEdit(Date.from(datePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            }
         });
+
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+    }
+
+    private LocalDate convertDateToLocaleDate(Date date) {
+        if (date != null) return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+
+        return null;
     }
 
     private Date getDate() {
