781df245a7dc4f5209d1a43caa33d2d7151a3514
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-11-17 09:05:40 +0300

    Rewrite the class in connection with the new gui.

--- src/main/java/org/accounting/forms/RolesForm.java
+++ src/main/java/org/accounting/forms/RolesForm.java
@@ -1,218 +1,87 @@
 package org.accounting.forms;
 
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.GridLayoutManager;
 import org.accounting.database.models.Role;
-import org.accounting.forms.helpers.YesNoDialog;
+import org.accounting.forms.helpers.AlertMessage;
 import org.accounting.forms.models.tablemodels.RoleFX;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
 
-public class RolesForm extends JDialog implements ActionListener {
-    private JPanel panelRolesForm;
-    private JTable tableRoles;
-    private JTextField textFieldRole;
-    private JTextField lookupCode;
-    private JButton addButton;
-    private JButton editButton;
-    private JButton saveButton;
-    private JButton cancelButton;
-    private JButton deleteButton;
-    private JLabel labelRole;
-    private JScrollPane scrollPaneTableRoles;
-    private RoleFX roleTableModel;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class RolesForm implements Initializable {
+    @FXML private TableView<RoleFX> tableRoles;
+    @FXML private TableColumn<RoleFX, String> columnRole;
+    @FXML private TableColumn<RoleFX, String> columnLookupCode;
+    @FXML private TextField tfNameRole;
+    @FXML private TextField tfLookupCode;
+    private ObservableList<RoleFX> data;
 
     public RolesForm() {
-        createForm();
+        data = FXCollections.observableArrayList();
+        ArrayList<Role> results = Role.getAll();
 
-       // roleTableModel = new RoleFX();
-
-       // Role.getAll().forEach(roleTableModel::addRecord);
-        tableRoles.getTableHeader().setReorderingAllowed(false);
-        //tableRoles.setModel(roleTableModel);
-
-        tableRoles.addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent mouseEvent) {
-                if (mouseEvent.getClickCount() == 2) {
-                    setValues();
-                }
-            }
-        });
-    }
-
-    private void createForm() {
-        setContentPane(panelRolesForm);
-        setSize(450, 300);
-        setLocationRelativeTo(null);
-        setTitle("Roles");
-        setModalityType(ModalityType.APPLICATION_MODAL);
-
-        addButton.addActionListener(this);
-        editButton.addActionListener(this);
-        saveButton.addActionListener(this);
-        cancelButton.addActionListener(this);
-        deleteButton.addActionListener(this);
-    }
-
-    private void insertRecord() {
-        Role role = new Role();
-        role.setName(textFieldRole.getText());
-        role.setLookupCode(lookupCode.getText());
-
-        if (!role.save()) {
-            JOptionPane.showMessageDialog(this, role.getErrors().fullMessages("\n"));
-            return;
+        for(Role role: results) {
+            data.add(new RoleFX(role));
         }
-
-       // roleTableModel.addRecord(role);
-        textFieldRole.setText("");
-        lookupCode.setText("");
-    }
-
-    private void saveRecord() {
-        int rowIndex = tableRoles.getSelectedRow();
-       // Role role = roleTableModel.getRecord(rowIndex);
-//        role.setName(textFieldRole.getText());
-//        role.setLookupCode(lookupCode.getText());
-
-//        if (!role.save()) {
-//            JOptionPane.showMessageDialog(this, role.getErrors().fullMessages("\n"));
-//            return;
-//        }
-//
-//        roleTableModel.setValueAt(role, rowIndex);
-        setDefaultMode();
-    }
-
-    private void deleteRecord() {
-        int rowIndex = tableRoles.getSelectedRow();
-        if (rowIndex < 0) {
-            JOptionPane.showMessageDialog(this, "Select an entry in the table!");
-            return;
-        }
-
-        if (new YesNoDialog("Are you sure you want to delete the record?", "Message").isPositive()) {
-//            roleTableModel.getRecord(rowIndex).delete();
-//            roleTableModel.removeRow(rowIndex);
-        }
-    }
-
-    private void setValues() {
-        int rowIndex = tableRoles.getSelectedRow();
-        if (rowIndex < 0) {
-            JOptionPane.showMessageDialog(this, "Select an entry in the table!");
-            return;
-        }
-
-////        Role role = roleTableModel.getRecord(rowIndex);
-//        textFieldRole.setText(role.getName());
-//        lookupCode.setText(role.getLookupCode());
-        setEditMode();
-    }
-
-    private void setDefaultMode() {
-        addButton.setEnabled(true);
-        editButton.setEnabled(true);
-        saveButton.setEnabled(false);
-        cancelButton.setEnabled(false);
-        deleteButton.setEnabled(true);
-        tableRoles.setEnabled(true);
-        textFieldRole.setText("");
-        lookupCode.setText("");
-    }
-
-    private void setEditMode() {
-        addButton.setEnabled(false);
-        editButton.setEnabled(false);
-        saveButton.setEnabled(true);
-        cancelButton.setEnabled(true);
-        deleteButton.setEnabled(false);
-        tableRoles.setEnabled(false);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()) {
-            case "addButton":
-                insertRecord();
-                break;
-            case "editButton":
-                setValues();
-                break;
-            case "saveButton":
-                saveRecord();
-                break;
-            case "cancelButton":
-                setDefaultMode();
-                break;
-            case "deleteButton":
-                deleteRecord();
-                break;
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        columnRole.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        columnLookupCode.setCellValueFactory(cellData -> cellData.getValue().lookupCodeProperty());
+
+        columnRole.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        tableRoles.setItems(data);
+    }
+
+    @FXML
+    private void handleBtnAdd() {
+        Role role = new Role();
+        role.setName(tfNameRole.getText());
+        role.setLookupCode(tfLookupCode.getText());
+
+        if (!role.save()) {
+            new AlertMessage("Error", role.getErrors().fullMessages()).showErrorMessage();
+            return;
+        }
+
+        data.add(new RoleFX(role));
+    }
+
+    @FXML
+    private void handleBtnAdd(Role role) {
+        if (!role.save()) {
+            new AlertMessage("Error", role.getErrors().fullMessages()).showErrorMessage();
         }
     }
 
-    {
-// GUI initializer generated by IntelliJ IDEA GUI Designer
-// >>> IMPORTANT!! <<<
-// DO NOT EDIT OR ADD ANY CODE HERE!
-        $$$setupUI$$$();
+    @FXML
+    private void handleBtnDelete() {
+        RoleFX roleFX = tableRoles.getSelectionModel().getSelectedItem();
+
+        if (new AlertMessage("Message","Are you sure you want to delete the record?").confirmationMessage()) {
+            roleFX.getRole().delete();
+        }
+
+        data.remove(roleFX);
     }
 
-    /**
-     * Method generated by IntelliJ IDEA GUI Designer
-     * >>> IMPORTANT!! <<<
-     * DO NOT edit this method OR call it in your code!
-     *
-     * @noinspection ALL
-     */
-    private void $$$setupUI$$$() {
-        panelRolesForm = new JPanel();
-        panelRolesForm.setLayout(new GridLayoutManager(4, 5, new Insets(5, 5, 5, 5), -1, -1));
-        scrollPaneTableRoles = new JScrollPane();
-        panelRolesForm.add(scrollPaneTableRoles, new GridConstraints(0, 0, 1, 5, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        tableRoles = new JTable();
-        scrollPaneTableRoles.setViewportView(tableRoles);
-        textFieldRole = new JTextField();
-        panelRolesForm.add(textFieldRole, new GridConstraints(2, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        labelRole = new JLabel();
-        labelRole.setText("Role");
-        panelRolesForm.add(labelRole, new GridConstraints(1, 0, 1, 5, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        addButton = new JButton();
-        addButton.setActionCommand("addButton");
-        addButton.setText("Add");
-        panelRolesForm.add(addButton, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        editButton = new JButton();
-        editButton.setActionCommand("editButton");
-        editButton.setText("Edit");
-        panelRolesForm.add(editButton, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        saveButton = new JButton();
-        saveButton.setActionCommand("saveButton");
-        saveButton.setEnabled(false);
-        saveButton.setText("Save");
-        panelRolesForm.add(saveButton, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        cancelButton = new JButton();
-        cancelButton.setActionCommand("cancelButton");
-        cancelButton.setEnabled(false);
-        cancelButton.setText("Cancel");
-        panelRolesForm.add(cancelButton, new GridConstraints(3, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        deleteButton = new JButton();
-        deleteButton.setActionCommand("deleteButton");
-        deleteButton.setText("Delete");
-        panelRolesForm.add(deleteButton, new GridConstraints(3, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        lookupCode = new JTextField();
-        panelRolesForm.add(lookupCode, new GridConstraints(2, 3, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-    }
-
-    /**
-     * @noinspection ALL
-     */
-    public JComponent $$$getRootComponent$$$() {
-        return panelRolesForm;
+    @FXML
+    private void onEditCommitName(TableColumn.CellEditEvent<RoleFX, String> roleFXStringCellEditEvent) {
+        RoleFX roleFX = tableRoles.getSelectionModel().getSelectedItem();
+        roleFX.setName(roleFXStringCellEditEvent.getNewValue());
+        roleFX.getRole().setName(roleFXStringCellEditEvent.getNewValue());
+        handleBtnAdd(roleFX.getRole());
     }
 
 }
