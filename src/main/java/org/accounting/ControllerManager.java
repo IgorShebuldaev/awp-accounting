63e36364e430af7283f074fb1be349b9a3588585
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-11-17 08:54:57 +0300

    Create a manager controller that stores and returns a stage instance for each controller.

---
+++ src/main/java/org/accounting/ControllerManager.java
@@ -0,0 +1,61 @@
+package org.accounting;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ControllerManager {
+    private HashMap<String, Stage> mapping = new HashMap<>();
+
+    private static ControllerManager instance = null;
+
+    public static ControllerManager getInstance() {
+        if (instance == null) {
+            instance = new ControllerManager();
+        }
+
+        return instance;
+    }
+
+    public Stage getStage(Class controller) {
+        return getStage_(controller, false);
+    }
+
+    public Stage getStageReloaded(Class controller) {
+        return getStage_(controller, true);
+    }
+
+    private Stage getStage_(Class controller, boolean reload) {
+        Stage stage = getStageFor(controller);
+
+        if (stage == null || reload) {
+            FXMLLoader loader = new FXMLLoader(controller.getResource(controller.getSimpleName() + ".fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scene scene = new Scene(root);
+
+            stage = new Stage();
+            stage.setScene(scene);
+
+            setStageFor(controller, stage);
+        }
+
+        return stage;
+    }
+
+    private Stage getStageFor(Class controller) {
+        return mapping.get(controller.getSimpleName());
+    }
+
+    private void setStageFor(Class controller, Stage stage) {
+        mapping.put(controller.getSimpleName(), stage);
+    }
+}
