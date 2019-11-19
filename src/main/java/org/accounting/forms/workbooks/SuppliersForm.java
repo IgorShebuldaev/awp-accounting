cbcda942e51547c96ee833624b81a1821fe4cd11
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-11-20 00:13:51 +0300

    Rewrite classes according to the new gui.

--- src/main/java/org/accounting/forms/workbooks/SuppliersForm.java
+++ src/main/java/org/accounting/forms/workbooks/SuppliersForm.java
@@ -1,214 +1,82 @@
 package org.accounting.forms.workbooks;
 
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.GridLayoutManager;
-import com.intellij.uiDesigner.core.Spacer;
 import org.accounting.database.models.Supplier;
+import org.accounting.forms.helpers.AlertMessage;
 import org.accounting.forms.models.tablemodels.SupplierFX;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
 
-public class SuppliersForm extends JPanel implements ActionListener {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-    private JPanel panelSuppliers;
-    private JScrollPane scrollPaneSuppliers;
-    private JTable tableSuppliers;
-    private JTextField textFieldSuppliersCompanyName;
-    private JLabel labelSuppliersCompanyName;
-    private JButton btnAddSuppliers;
-    private JButton btnEditSuppliers;
-    private JButton btnSaveSuppliers;
-    private JButton btnCancelSuppliers;
-    private JButton btnDeleteSuppliers;
-    private SupplierFX supplierFXModel;
+public class SuppliersForm implements Initializable {
+    @FXML private TableView<SupplierFX> tableSuppliers;
+    @FXML private TableColumn<SupplierFX, String> columnCompanyName;
+    @FXML private TextField tfCompanyName;
+    private ObservableList<SupplierFX> data;
 
-    SuppliersForm() {
-        super();
+    public SuppliersForm() {
+        data = FXCollections.observableArrayList();
+        ArrayList<Supplier> results = Supplier.getAll();
 
-        //supplierFXModel = new SupplierFX();
-
-        //Supplier.getAll().forEach(supplierFXModel::addRecord);
-        //tableSuppliers.setModel(supplierFXModel);
-        tableSuppliers.getTableHeader().setReorderingAllowed(false);
-
-        tableSuppliers.addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent mouseEvent) {
-                if (mouseEvent.getClickCount() == 2) {
-                    setValues();
-                }
-            }
-        });
-
-        createForm();
-    }
-
-    private void createForm() {
-        btnAddSuppliers.addActionListener(this);
-        btnDeleteSuppliers.addActionListener(this);
-        btnEditSuppliers.addActionListener(this);
-        btnSaveSuppliers.addActionListener(this);
-        btnCancelSuppliers.addActionListener(this);
-
-        add(panelSuppliers);
-    }
-
-    JPanel getPanel() {
-        return panelSuppliers;
-    }
-
-    private void insertRecord() {
-        Supplier supplier = new Supplier();
-        supplier.setName(textFieldSuppliersCompanyName.getText());
-
-        if (!supplier.save()) {
-            JOptionPane.showMessageDialog(this, supplier.getErrors().fullMessages("\n"));
-            return;
+        for(Supplier supplier: results) {
+            data.add(new SupplierFX(supplier));
         }
-
-        //supplierFXModel.addRecord(supplier);
-        textFieldSuppliersCompanyName.setText("");
-    }
-
-    private void saveRecord() {
-        int rowIndex = tableSuppliers.getSelectedRow();
-       // Supplier supplier = supplierFXModel.getRecord(rowIndex);
-       // supplier.setName(textFieldSuppliersCompanyName.getText());
-
-//        if (!supplier.save()) {
-//            JOptionPane.showMessageDialog(this, supplier.getErrors().fullMessages("\n"));
-//            return;
-//        }
-
-//        supplierFXModel.setValueAt(supplier, rowIndex);
-//        textFieldSuppliersCompanyName.setText("");
-//        setDefaultMode();
-    }
-
-    private void deleteRecord() {
-        int rowIndex = tableSuppliers.getSelectedRow();
-        if (rowIndex < 0) {
-            JOptionPane.showMessageDialog(null, "Select an entry in the table!");
-            return;
-        }
-
-//        if (new YesNoDialog("Are you sure you want to delete the record?", "Message").isPositive()) {
-//            supplierFXModel.getRecord(rowIndex).delete();
-//            supplierFXModel.removeRow(rowIndex);
-//        }
-    }
-
-    private void setValues() {
-        int rowIndex = tableSuppliers.getSelectedRow();
-        if (rowIndex < 0) {
-            JOptionPane.showMessageDialog(null, "Select an entry in the table!");
-            return;
-        }
-
-//        textFieldSuppliersCompanyName.setText(supplierFXModel.getRecord(rowIndex).getName());
-//        setEditMode();
-    }
-
-    private void setDefaultMode() {
-        btnAddSuppliers.setEnabled(true);
-        btnEditSuppliers.setEnabled(true);
-        btnDeleteSuppliers.setEnabled(true);
-        tableSuppliers.setEnabled(true);
-        btnCancelSuppliers.setEnabled(false);
-        btnSaveSuppliers.setEnabled(false);
-        textFieldSuppliersCompanyName.setText("");
-    }
-
-    private void setEditMode() {
-        btnAddSuppliers.setEnabled(false);
-        btnEditSuppliers.setEnabled(false);
-        btnDeleteSuppliers.setEnabled(false);
-        tableSuppliers.setEnabled(false);
-        btnCancelSuppliers.setEnabled(true);
-        btnSaveSuppliers.setEnabled(true);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()) {
-            case "btnAddSupplier":
-                insertRecord();
-                break;
-            case "btnEditSupplier":
-                setValues();
-                break;
-            case "btnSaveSupplier":
-                saveRecord();
-                break;
-            case "btnCancelSupplier":
-                setDefaultMode();
-                break;
-            case "btnDeleteSupplier":
-                deleteRecord();
-                break;
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        columnCompanyName.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+
+        columnCompanyName.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        tableSuppliers.setItems(data);
+    }
+
+    @FXML
+    private void handleBtnAdd() {
+        Supplier supplier = new Supplier();
+        supplier.setName(tfCompanyName.getText());
+
+        if (!supplier.save()) {
+            new AlertMessage("Error", supplier.getErrors().fullMessages()).showErrorMessage();
+            return;
+        }
+
+        data.add(new SupplierFX(supplier));
+    }
+
+    @FXML
+    private void handleBtnAdd(Supplier supplier) {
+        if (!supplier.save()) {
+            new AlertMessage("Error", supplier.getErrors().fullMessages()).showErrorMessage();
         }
     }
 
-    {
-// GUI initializer generated by IntelliJ IDEA GUI Designer
-// >>> IMPORTANT!! <<<
-// DO NOT EDIT OR ADD ANY CODE HERE!
-        $$$setupUI$$$();
+    @FXML
+    private void handleBtnDelete() {
+        SupplierFX supplierFX = tableSuppliers.getSelectionModel().getSelectedItem();
+
+        if (new AlertMessage("Message","Are you sure you want to delete the record?").showConfirmationMessage()) {
+            supplierFX.getSupplier().delete();
+            data.remove(supplierFX);
+        }
     }
 
-    /**
-     * Method generated by IntelliJ IDEA GUI Designer
-     * >>> IMPORTANT!! <<<
-     * DO NOT edit this method OR call it in your code!
-     *
-     * @noinspection ALL
-     */
-    private void $$$setupUI$$$() {
-        final JPanel panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1, true, true));
-        panelSuppliers = new JPanel();
-        panelSuppliers.setLayout(new GridLayoutManager(4, 8, new Insets(5, 5, 5, 5), -1, -1));
-        panel1.add(panelSuppliers, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        scrollPaneSuppliers = new JScrollPane();
-        panelSuppliers.add(scrollPaneSuppliers, new GridConstraints(0, 0, 1, 8, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        tableSuppliers = new JTable();
-        scrollPaneSuppliers.setViewportView(tableSuppliers);
-        textFieldSuppliersCompanyName = new JTextField();
-        panelSuppliers.add(textFieldSuppliersCompanyName, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        labelSuppliersCompanyName = new JLabel();
-        labelSuppliersCompanyName.setText("Company name");
-        panelSuppliers.add(labelSuppliersCompanyName, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        btnAddSuppliers = new JButton();
-        btnAddSuppliers.setActionCommand("btnAddSupplier");
-        btnAddSuppliers.setText("Add");
-        panelSuppliers.add(btnAddSuppliers, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        btnEditSuppliers = new JButton();
-        btnEditSuppliers.setActionCommand("btnEditSupplier");
-        btnEditSuppliers.setText("Edit");
-        panelSuppliers.add(btnEditSuppliers, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        btnSaveSuppliers = new JButton();
-        btnSaveSuppliers.setActionCommand("btnSaveSupplier");
-        btnSaveSuppliers.setEnabled(false);
-        btnSaveSuppliers.setText("Save");
-        panelSuppliers.add(btnSaveSuppliers, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        btnCancelSuppliers = new JButton();
-        btnCancelSuppliers.setActionCommand("btnCancelSupplier");
-        btnCancelSuppliers.setEnabled(false);
-        btnCancelSuppliers.setText("Cancel");
-        panelSuppliers.add(btnCancelSuppliers, new GridConstraints(3, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        btnDeleteSuppliers = new JButton();
-        btnDeleteSuppliers.setActionCommand("btnDeleteSupplier");
-        btnDeleteSuppliers.setText("Delete");
-        panelSuppliers.add(btnDeleteSuppliers, new GridConstraints(3, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final Spacer spacer1 = new Spacer();
-        panelSuppliers.add(spacer1, new GridConstraints(3, 7, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, new Dimension(22, 11), null, 0, false));
-        final Spacer spacer2 = new Spacer();
-        panelSuppliers.add(spacer2, new GridConstraints(3, 6, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        final Spacer spacer3 = new Spacer();
-        panelSuppliers.add(spacer3, new GridConstraints(3, 5, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+    @FXML
+    private void onEditCommitCompanyName(TableColumn.CellEditEvent<SupplierFX, String> supplierFXStringCellEditEvent) {
+        SupplierFX supplierFX = tableSuppliers.getSelectionModel().getSelectedItem();
+        supplierFX.setName(supplierFXStringCellEditEvent.getNewValue());
+        supplierFX.getSupplier().setName(supplierFXStringCellEditEvent.getNewValue());
+        handleBtnAdd(supplierFX.getSupplier());
     }
+
 }
---
+++ src/main/java/org/accounting/forms/workbooks/WorkersForm.java
@@ -0,0 +1,178 @@
+package org.accounting.forms.workbooks;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
+import org.accounting.database.models.Position;
+import org.accounting.database.models.Worker;
+import org.accounting.forms.helpers.AlertMessage;
+import org.accounting.forms.models.DateTableCell;
+import org.accounting.forms.models.comboboxcell.PositionComboBoxCell;
+import org.accounting.forms.models.tablemodels.PositionFX;
+import org.accounting.forms.models.tablemodels.WorkerFX;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class WorkersForm implements Initializable {
+    @FXML private AnchorPane paneWorkers;
+    @FXML private TableView<WorkerFX> tableWorkers;
+    @FXML private TableColumn<WorkerFX, String> columnFullName;
+    @FXML private TableColumn<WorkerFX, Date> columnDateOfBirth;
+    @FXML private TableColumn<WorkerFX, String> columnPosition;
+    @FXML private TableColumn<WorkerFX, String> columnEmail;
+    @FXML private TextField tfFullName;
+    @FXML private DatePicker dpDateOfBirth;
+    @FXML private ComboBox<PositionFX> cbPosition;
+    private ObservableList<WorkerFX> data;
+
+    public WorkersForm() {
+        data = FXCollections.observableArrayList();
+        ArrayList<Worker> results = Worker.getAll();
+
+        for(Worker worker: results) {
+            data.add(new WorkerFX(worker));
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        columnFullName.setCellValueFactory(cellData -> cellData.getValue().fullNameProperty());
+        columnDateOfBirth.setCellValueFactory(cellData -> cellData.getValue().dateOfBirthProperty());
+        columnPosition.setCellValueFactory(cellData -> cellData.getValue().positionProperty());
+        columnEmail.setCellValueFactory(cellData -> cellData.getValue().userNameProperty());
+
+        columnFullName.setCellFactory(TextFieldTableCell.forTableColumn());
+        columnDateOfBirth.setCellFactory((TableColumn<WorkerFX, Date> param) -> new DateTableCell<>());
+        columnPosition.setCellFactory((TableColumn<WorkerFX, String> param) -> new PositionComboBoxCell(getItemsComboBoxPosition()));
+
+        tableWorkers.setItems(data);
+    }
+
+    private void setPropertiesCombobox() {
+        cbPosition.setCellFactory(new Callback<ListView<PositionFX>, ListCell<PositionFX>>() {
+            @Override
+            public ListCell<PositionFX> call(ListView<PositionFX> positionFXListView) {
+                return new ListCell<PositionFX>() {
+                    @Override
+                    protected void updateItem(PositionFX item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item == null || empty) {
+                            setGraphic(null);
+                        } else {
+                            setText(item.getName());
+                        }
+                    }
+                };
+            }
+        });
+
+        cbPosition.setConverter(new StringConverter<PositionFX>() {
+            @Override
+            public String toString(PositionFX positionFX) {
+                if (positionFX == null){
+                    return null;
+                } else {
+                    return positionFX.getName();
+                }
+            }
+
+            @Override
+            public PositionFX fromString(String str) {
+                return null;
+            }
+        });
+    }
+
+    private ObservableList<PositionFX> getItemsComboBoxPosition() {
+        ObservableList<PositionFX> workers = FXCollections.observableArrayList();
+        ArrayList<Position> results = Position.getAll();
+
+        for (Position position : results) {
+            workers.add(new PositionFX(position));
+        }
+
+        return workers;
+    }
+
+    private void clearComponents() {
+        tfFullName.clear();
+        dpDateOfBirth.setValue(null);
+        cbPosition.getSelectionModel().clearSelection();
+    }
+
+    private Date convertToDate(LocalDate dateToConvert) {
+        if (dateToConvert != null) {
+            return Date.from(dateToConvert.atStartOfDay()
+                    .atZone(ZoneId.systemDefault())
+                    .toInstant());
+        }
+        return null;
+    }
+
+    @FXML
+    private void handleBtnAdd() {
+        Worker supplier = new Worker();
+        //TODO:buildUser();
+        supplier.setFullName(tfFullName.getText());
+        supplier.setDateOfBirth(convertToDate(dpDateOfBirth.getValue()));
+        supplier.setPositionID(cbPosition.getSelectionModel().getSelectedItem().getPosition().getId());
+
+        if (!supplier.save()) {
+            new AlertMessage("Error", supplier.getErrors().fullMessages()).showErrorMessage();
+            return;
+        }
+
+        data.add(new WorkerFX(supplier));
+        clearComponents();
+    }
+
+    @FXML
+    private void handleBtnAdd(Worker worker) {
+        if (!worker.save()) {
+            new AlertMessage("Error", worker.getErrors().fullMessages()).showErrorMessage();
+        }
+    }
+
+    @FXML
+    private void handleBtnDelete() {
+        WorkerFX workerFX = tableWorkers.getSelectionModel().getSelectedItem();
+
+        if (new AlertMessage("Message","Are you sure you want to delete the record?").showConfirmationMessage()) {
+            workerFX.getWorker().delete();
+            data.remove(workerFX);
+        }
+    }
+
+    @FXML
+    private void onEditCommitFullName(TableColumn.CellEditEvent<WorkerFX, String> workerFXStringCellEditEvent) {
+        WorkerFX workerFX = tableWorkers.getSelectionModel().getSelectedItem();
+        workerFX.setFullName(workerFXStringCellEditEvent.getNewValue());
+        workerFX.getWorker().setFullName(workerFXStringCellEditEvent.getNewValue());
+        handleBtnAdd(workerFX.getWorker());
+    }
+
+    @FXML
+    private void onEditCommitDateOfBirth(TableColumn.CellEditEvent<WorkerFX, Date> workerFXDateCellEditEvent) {
+        WorkerFX workerFX = tableWorkers.getSelectionModel().getSelectedItem();
+        workerFX.setDateOfBirth(workerFXDateCellEditEvent.getNewValue());
+        workerFX.getWorker().setDateOfBirth(workerFXDateCellEditEvent.getNewValue());
+        handleBtnAdd(workerFX.getWorker());
+    }
+
+    @FXML
+    private void onEditCommitPosition(TableColumn.CellEditEvent<WorkerFX, String> workerFXStringCellEditEvent) {
+        //TODO:save value from comboboxcells
+    }
+
+}
